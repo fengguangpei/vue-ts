5b70593a12ea1d2d49e02733b8887d2108c357ad
fenggp <18319407429@163.com>
2024-06-10 17:02:41 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,43 @@ const sub = () => count.value = count.value + 2
 
 <template>
   <div class="count">
-    <h1>{{ count }}</h1>
-    <button @click="add">+</button>
-    <button @click="sub">-</button>
+    <div class="header">
+      This is Header
+    </div>
+    <div class="main">
+      This is Main
+    </div>
+    <div class="footer">
+      This is Footer
+    </div>
   </div>
 </template>
 
 <style scoped>
 .count {
-  margin: 200px auto;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
 
-  button+button {
-    margin-left: 10px;
+  .header {
+    height: 100px;
+    border-bottom: 1px solid #ccc;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .main {
+    flex: 1;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  .footer {
+    border-top: 1px solid #ccc;
+    height: 100px;
+    text-align: center;
+    vertical-align: middle;
   }
 }
 </style>
